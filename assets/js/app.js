0ecbcf58e8f0b2327ca8cf8e721190c4d6748000
khoubaib <khoubaib15@outlook.com>
2023-01-06 16:03:16 +0000

    added some js

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+var apiKey = '6e2c9a825d52912ed09435216f712368';
+var baseUrl = 'https://api.openweathermap.org/data/2.5/';
+var currentWeather = baseUrl + 'weather?&appid=' + apiKey + '&units=metric&';
+var forcast = baseUrl + 'forecast?&appid=' + apiKey + '&units=metric&';
+
+console.log('Current weather link: ', currentWeather);
+console.log('Forecast current weather link: ', forcast);
