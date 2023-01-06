6d17b1383087c1c84bfcde9cbe4bc5a9cacb866b
khoubaib <khoubaib15@outlook.com>
2023-01-06 16:11:18 +0000

    added output for weather

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,12 +7,34 @@ console.log('Current weather link: ', currentWeather);
 console.log('Forecast current weather link: ', forcast);
 
 function getLocalStoredItems() {
-    var getStoredItem = JSON.parse(localStorage.getItem("SearchKey")) || [];
-    return getStoredItem;
-  }
-  
-  function storeItemsLocally(searchItem) {
-    var saveItem = localStorage.setItem("SearchKey", JSON.stringify(searchItem));
-    return saveItem;
-  }
-  
+  var getStoredItem = JSON.parse(localStorage.getItem("SearchKey")) || [];
+  return getStoredItem;
+}
+
+function storeItemsLocally(searchItem) {
+  var saveItem = localStorage.setItem("SearchKey", JSON.stringify(searchItem));
+  return saveItem;
+}
+
+function DOMCurrentWeather(searchInput) {
+  var todaysDate = moment(new Date()).format("DD/MM/YYYY");
+  var weatherForToday = $('#today');
+  $.get(currentWeather + `q=${searchInput}`)
+    .then(currData => {
+
+      var iconcode = currData.weather[0].icon;
+      var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+      var lat = currData.coord.lat;
+      var lon = currData.coord.lon;
+      var currDOMoutPut = `    
+            <h5 class="card-title">${currData.name} (${todaysDate}) <img src='${iconurl}'></h5>
+            <p>Temp: ${Math.round(currData.main.temp)}
+            <span>&#176;</span>C</p>
+            <p>Wind:  ${parseFloat(currData.wind.speed).toPrecision(2)} KPH</p>
+            <p>Humidity: ${Math.round(currData.main.humidity)}%</p>
+            `
+      weatherForToday.text(" ");
+      weatherForToday.append(currDOMoutPut);
+      getForeCast(lat, lon);
+    })
+}
